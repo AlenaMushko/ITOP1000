5fa0302cddd4388de2752560792174a531ab8086
alena <108529764+AlenaMushko@users.noreply.github.com>
2024-01-01 00:38:26 +0200

    update: cache for api

--- src/app/services/monobank.service.ts
+++ src/app/services/monobank.service.ts
@@ -1,37 +1,43 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
-import { Observable, of } from "rxjs";
+import {Observable, tap, throwError} from "rxjs";
+import { map, catchError, shareReplay } from 'rxjs/operators';
 
 import { baseUrl, codeConstant } from "../constants";
 import { ICours, ICoursMono } from "../interfaces/cours.interface";
-import { map } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
 })
 export class MonobankService {
-    private cache: ICours[] | null = null;
-    private cacheUpdateTime: Date | null = null;
-    private cacheExpirationMs = 60 * 60 * 1000;
+    private cache$: Observable<ICours[]> | null = null;
+    private lastUpdate: Date | null = null;
+    private cacheLifeTime = 60 * 60 *1000;
 
     constructor(private httpClient: HttpClient) {}
 
     getCurrencies(): Observable<ICours[]> {
         const now = new Date();
-        if (this.cache && this.cacheUpdateTime && (now.getTime() - this.cacheUpdateTime.getTime()) < this.cacheExpirationMs) {
-            return of(this.cache);
-        } else {
-            return this.httpClient.get<ICoursMono[]>(baseUrl)
-                .pipe(
-                    map(data => {
-                        const processedData = this.processCurrencies(data);
-                        this.cache = processedData;
-                        this.cacheUpdateTime = new Date();
-                        return processedData;
-                    })
-                );
+
+        if (this.cache$ && this.lastUpdate && (now.getTime() - (this.lastUpdate.getTime() + this.cacheLifeTime)) <= 0){
+            return this.cache$;
         }
+
+        if (!this.cache$) {
+            this.cache$ = this.httpClient.get<ICoursMono[]>(baseUrl).pipe(
+                map(data => this.processCurrencies(data)),
+                tap(() => this.lastUpdate = new Date()),
+                shareReplay(1),
+                catchError(err => {
+                    this.cache$ = null;
+                    return throwError(err);
+                })
+            );
+        }
+
+        return this.cache$;
     }
+
     private processCurrencies(data: ICoursMono[]): ICours[] {
         return data.reduce<ICours[]>((acc, value) => {
             const {currencyCodeB, currencyCodeA, rateBuy, rateSell} = value;
