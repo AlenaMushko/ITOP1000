d98e65191b4222fb906d681010205cb31bf077c9
alena <108529764+AlenaMushko@users.noreply.github.com>
2023-12-31 23:31:09 +0200

    add: cache for api

--- src/app/services/monobank.service.ts
+++ src/app/services/monobank.service.ts
@@ -1,22 +1,37 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {baseUrl, codeConstant} from "../constants";
-import {ICours, ICoursMono} from "../interfaces/cours.interface";
-import {map, Observable} from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { Observable, of } from "rxjs";
+
+import { baseUrl, codeConstant } from "../constants";
+import { ICours, ICoursMono } from "../interfaces/cours.interface";
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class MonobankService {
-  constructor(private httpClient : HttpClient) {}
+    private cache: ICours[] | null = null;
+    private cacheUpdateTime: Date | null = null;
+    private cacheExpirationMs = 60 * 60 * 1000;
+
+    constructor(private httpClient: HttpClient) {}
 
     getCurrencies(): Observable<ICours[]> {
-        return this.httpClient.get<ICoursMono[]>(baseUrl)
-            .pipe(
-                map(data => this.processCurrencies(data))
-            );
+        const now = new Date();
+        if (this.cache && this.cacheUpdateTime && (now.getTime() - this.cacheUpdateTime.getTime()) < this.cacheExpirationMs) {
+            return of(this.cache);
+        } else {
+            return this.httpClient.get<ICoursMono[]>(baseUrl)
+                .pipe(
+                    map(data => {
+                        const processedData = this.processCurrencies(data);
+                        this.cache = processedData;
+                        this.cacheUpdateTime = new Date();
+                        return processedData;
+                    })
+                );
+        }
     }
-
     private processCurrencies(data: ICoursMono[]): ICours[] {
         return data.reduce<ICours[]>((acc, value) => {
             const {currencyCodeB, currencyCodeA, rateBuy, rateSell} = value;
